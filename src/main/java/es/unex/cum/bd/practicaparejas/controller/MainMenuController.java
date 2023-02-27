5716e990cef7890f7e55583e71a9d02c7ffb9c6b
Kyantti <psetraki@alumnos.unex.es>
2023-02-27 18:38:01 +0100

    primer commit

---
+++ src/main/java/es/unex/cum/bd/practicaparejas/controller/MainMenuController.java
@@ -0,0 +1,21 @@
+package main.java.es.unex.cum.bd.practicaparejas.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+
+public class MainMenuController {
+
+    @FXML
+    private Font x1;
+
+    @FXML
+    private Color x2;
+
+    @FXML
+    private Font x3;
+
+    @FXML
+    private Color x4;
+
+}

087faadc23539ed375951a5a40858d1cae153572
Pablo <pablosetra@icloud.com>
2023-04-17 12:44:08 +0200

    completar MssqlServiceDao y hacer otra entidad

--- src/main/java/es/unex/cum/bd/practicapareja/model/dao/AscriptionDao.java
+++
@@ -1,7 +0,0 @@
-package es.unex.cum.bd.practicapareja.model.dao;
-
-import es.unex.cum.bd.practicapareja.model.entities.Ascription;
-
-public interface AscriptionDao extends Dao <Ascription, Integer> {
-    
-}
